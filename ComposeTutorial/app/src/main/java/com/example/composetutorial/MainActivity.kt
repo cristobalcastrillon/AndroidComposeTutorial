9f6ff56966c16f8ec2d18efd5293f70c186b685f
Nestor Diazgranados <nestor.pabon@care.com>
2022-05-31 12:28:50 -0500

    Dark theme added.

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,12 +23,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            ComposeTutorialTheme() {
+                MessageCard(Message(author = "Cristóbal", body = "I'm learning Compose!"))
+            }
         }
     }
 }
@@ -37,6 +44,11 @@ fun Greeting(name: String) {
 }
 
 @Composable
+fun PreviewGreeting() {
+    Greeting("Android")
+}
+
+@Composable
 fun MessageCard(msg: Message){
     Row (modifier = Modifier.padding(all = 8.dp)) {
         Image(
@@ -45,24 +57,36 @@ fun MessageCard(msg: Message){
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(2.dp))
-            Text(text = msg.body)
+            Surface (shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
-@Preview
-@Composable
-fun PreviewGreeting() {
-    Greeting("Android")
-}
-
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard(){
-    MessageCard(Message(author = "Cristóbal", body = "I'm learning Compose!"))
+    ComposeTutorialTheme() {
+        MessageCard(Message(author = "Cristóbal", body = "I'm learning Compose!"))
+    }
 }
